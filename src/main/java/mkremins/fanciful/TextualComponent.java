6125e3b36421f531b123fd40e203e7f228624144
Glen Husman <wappokickingdeer@gmail.com>
2014-06-07 08:41:47 -0700

    Added yet unsupported TextualComponents These methods are implemented as per the link below, but as they are snapshot-only, they throw exceptions. However, the implementation is present such that removing one line and one paragraph from the docs of each method will make them complete and snapshot-client-compatible.
    http://www.minecraftforum.net/topic/1975697-1718-raw-json-text-examples-for-tellraw-title-update14w20b/#SYNTAXtellraw

--- src/main/java/mkremins/fanciful/TextualComponent.java
+++ src/main/java/mkremins/fanciful/TextualComponent.java
@@ -1,6 +1,7 @@
 package mkremins.fanciful;
 
 import java.io.IOException;
+import java.io.StringWriter;
 
 import org.bukkit.craftbukkit.libs.com.google.gson.stream.JsonWriter;
 
@@ -29,7 +30,7 @@ public abstract class TextualComponent {
 	 * @param writer The object to which to write the JSON data.
 	 * @throws IOException If an error occurs while writing to the stream.
 	 */
-	public void writeToJSON(JsonWriter writer) throws IOException{
+	public void writeJson(JsonWriter writer) throws IOException{
 		writer.name(getKey()).value(getValue());
 	}
 	
@@ -91,5 +92,64 @@ public abstract class TextualComponent {
 	 */
 	public static TextualComponent createTranslateString(String translateKey){
 		return new ArbitraryTextTypeComponent("translate", translateKey);
+	}
+	
+	private static final void throwUnsupportedSnapshot(){
+		throw new UnsupportedOperationException("This feature is only supported in snapshot releases.");
+	}
+	
+	/**
+	 * Create a textual component representing a scoreboard value.
+	 * The client will see their own score for the specified objective as the text represented by this component.
+	 * <p>
+	 * <b>This method is currently guaranteed to throw an {@code UnsupportedOperationException} as it is only supported on snapshot clients.</b>
+	 * </p>
+	 * @param scoreboardObjective The name of the objective for which to display the score.
+	 * @return The text component representing the specified scoreboard score (for the viewing player), or {@code null} if an error occurs during JSON serialization.
+	 */
+	public static TextualComponent createScoreValueString(String scoreboardObjective){
+		return createScoreValueString("*", scoreboardObjective);
+	}
+	
+	/**
+	 * Create a textual component representing a scoreboard value.
+	 * The client will see the score of the specified player for the specified objective as the text represented by this component.
+	 * <p>
+	 * <b>This method is currently guaranteed to throw an {@code UnsupportedOperationException} as it is only supported on snapshot clients.</b>
+	 * </p>
+	 * @param playerName The name of the player whos score will be shown. If this string represents the single-character sequence "*", the viewing player's score will be displayed.
+	 * Standard minecraft selectors (@a, @p, etc) are <em>not</em> supported.
+	 * @param scoreboardObjective The name of the objective for which to display the score.
+	 * @return The text component representing the specified scoreboard score for the specified player, or {@code null} if an error occurs during JSON serialization.
+	 */
+	public static TextualComponent createScoreValueString(String playerName, String scoreboardObjective){
+		throwUnsupportedSnapshot(); // Remove this line when the feature is released to non-snapshot versions, in addition to updating ALL THE OVERLOADS documentation accordingly
+		
+		StringWriter string = new StringWriter();
+		JsonWriter json = new JsonWriter(string);
+		// Write the JSON string, required for this component, out
+		try {
+			json.beginObject().name("name").value(playerName)
+			.name("objective").value(scoreboardObjective).endObject().close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return new ArbitraryTextTypeComponent("score", string.toString());
+	}
+	
+	/**
+	 * Create a textual component representing a player name, retrievable by using a standard minecraft selector.
+	 * The client will see the players or entities captured by the specified selector as the text represented by this component.
+	 * <p>
+	 * <b>This method is currently guaranteed to throw an {@code UnsupportedOperationException} as it is only supported on snapshot clients.</b>
+	 * </p>
+	 * @param selector The minecraft player or entity selector which will capture the entities whose string representations will be displayed in the place of this text component.
+	 * @return The text component representing the name of the entities captured by the selector.
+	 */
+	public static TextualComponent createSelectorString(String selector){
+		throwUnsupportedSnapshot(); // Remove this line when the feature is released to non-snapshot versions, in addition to updating ALL THE OVERLOADS documentation accordingly
+		
+		return new ArbitraryTextTypeComponent("selector", selector);
 	}
 }
